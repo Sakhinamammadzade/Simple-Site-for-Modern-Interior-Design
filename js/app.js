486181549a72f5fa6df1113772ce42f50fb10d73
M.Sakina <sakhinamammadzade0406@gmail.com>
2022-07-20 02:47:45 +0400

    simple site for Modern Interior Design

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+var barIcon=document.getElementById("bar-ico")
+var ResponsiveMenu=document.querySelector("#header .responsive-menu")
+
+barIcon.onclick=function(){
+   ResponsiveMenu.classList.toggle("active-menu")
+}
+
+
+
+ 
+      
+
+
+
+  
